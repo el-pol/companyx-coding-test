bae88698d04e54e26f19bb5246417d9bde5038ea
Pol Milian <pol@vidjet.io>
2022-03-17 20:26:34 +0100

    Added type, sorting in place

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,12 @@ import Job from "./components/Job";
 import Nav from "./components/Nav";
 import OrderBy from "./components/OrderBy";
 import { OrderTypes } from "./types/order";
+import JobDefinition from "./types/job";
 
 import "./App.css";
 
 const App: React.FC = () => {
-  const [jobs, setJobs] = useState([]);
+  const [jobs, setJobs] = useState<JobDefinition[]>([]);
   const [order, setOrder] = useState<OrderTypes>(OrderTypes.Random)
 
   const fetchData: () => Promise<void> = async () => {
@@ -26,13 +27,18 @@ const App: React.FC = () => {
     return <Job key={id} {...value} />;
   });
 
+  const SortedJobList: React.ReactElement[] = jobs.sort((a, b) => a.priority - b.priority).map((value) => {
+    const { id } = value;
+    return <Job key={id} {...value} />;
+  });
+
   return (
     <div className="App">
         <Nav />
         {!!JobList.length && (
           <div data-testid="app-jobs" className="App-jobs">
             <OrderBy onOrderSelection={setOrder} />
-            {JobList}
+            {order === OrderTypes.Random ? JobList : SortedJobList}
           </div>
         )}
     </div>
