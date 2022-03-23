548f6459110b12f0a5c00d723d378ab8a81cee92
Pol Milian <pol.milian.dev@gmail.com>
2022-03-23 14:43:47 +0700

    Added pagination

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,19 @@ import Nav from "./components/Nav";
 import OrderBy from "./components/OrderBy";
 import { OrderTypes } from "./types/order";
 import JobDefinition from "./types/job";
+import ReactPaginate from 'react-paginate';
+
 
 import "./App.css";
 import JobList from "./components/JobList";
 
 const App: React.FC = () => {
   const [jobs, setJobs] = useState<JobDefinition[]>([]);
+  const [currentJobs, setCurrentJobs] = useState<JobDefinition[]>([]);
   const [orderBy, setOrderBy] = useState<OrderTypes>(OrderTypes.Random)
   const [isLoading, setIsLoading] = useState(true)
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
 
   const fetchData: () => Promise<void> = async () => {
     setIsLoading(true)
@@ -20,11 +25,27 @@ const App: React.FC = () => {
     setIsLoading(false)
     setJobs(data);
   };
+  const itemsPerPage = 10
 
   useEffect(() => {
-    setTimeout(() => fetchData(), 3000);
+    fetchData();
   }, []);
 
+  useEffect(() => {
+    // Fetch items from another resources.
+    const endOffset = itemOffset + itemsPerPage;
+    console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+    setCurrentJobs(jobs.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(jobs.length / itemsPerPage));
+  }, [itemOffset, itemsPerPage, jobs]);
+
+  const handlePageClick = (event: any) => {
+    const newOffset = (event.selected * itemsPerPage) % jobs.length;
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`
+    );
+    setItemOffset(newOffset);
+  };
   
 
   return (
@@ -34,9 +55,17 @@ const App: React.FC = () => {
         {!!jobs.length && (
           <div data-testid="app-jobs" className="App-jobs">
             <OrderBy onOrderSelection={setOrderBy} />
-            <JobList jobs={jobs} orderBy={orderBy} />
+            <JobList jobs={currentJobs} orderBy={orderBy} />
           </div>
         )}
+        <ReactPaginate
+          breakLabel="..."
+          nextLabel="next >"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={5}
+          pageCount={pageCount}
+          previousLabel="< previous"
+        />
     </div>
   );
 };
