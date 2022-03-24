0db07586ac994ebb6d721f7843b18c16eba75af3
Pol Milian <pol.milian.dev@gmail.com>
2022-03-24 14:12:30 +0700

    Made the cursor a pointer

--- src/App.tsx
+++ src/App.tsx
@@ -67,6 +67,7 @@ const App: React.FC = () => {
           previousLabel="< previous"
           containerClassName="Pagination-container"
           activeClassName="Pagination-active"
+          pageClassName="Pagination-element"
         />
     </div>
   );
