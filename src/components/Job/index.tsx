ee011dd9be3615bbc1375bba876cc9d69311cbd2
Pol Milian <pol.milian.dev@gmail.com>
2022-03-25 01:11:50 +0700

    Fixed wrong markup

--- src/components/Job/index.tsx
+++ src/components/Job/index.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 
 const Job: FC<JobProps> = ({ description, role, url, city, company, id }) => {
   return (
-    <article className="App-job" id={id}>
+    <li className="App-job" id={id}>
       <a className="App-job__link" href={url} target="_blank" rel="noreferrer">
         <h2 className="App-job__title">{role}</h2>
         <span className="App-job__meta">
@@ -14,7 +14,7 @@ const Job: FC<JobProps> = ({ description, role, url, city, company, id }) => {
         </span>
         <p className="App-job__abstract">{description}</p>
       </a>
-    </article>
+    </li>
   );
 };
 
